d0a25585d857dcf2318f6437a959a64ff6161030
Sophie Townsend <se.townsend31@gmail.com>
2021-06-28 15:50:25 +1000

    reset password

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4567
 
-app.get('/', (req, res) => res.send('hello world today is cold'))
+app.get('/', (req, res) => res.send('hello world'))
+
+app.get('/reset-password', (req, res) => res.send('reset password'))
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
