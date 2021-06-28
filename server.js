4da0972e63757d8aa4a7b905167b8a7e1f839574
Sophie Townsend <se.townsend31@gmail.com>
2021-06-28 15:45:36 +1000

    update res.send response

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 4567
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hello world today is cold'))
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
